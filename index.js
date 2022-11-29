8a494530380def1ad95fc86a47c35279652d6abe
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2022-11-29 17:54:18 +0530

    updated-f2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,15 +12,15 @@ const userRouter = require("./routes/userRoutes");
 app.use(cors());
 //added code ashif
 app.get('/',(req,res)=>{
-  res.send("hey server is surring succesfully");
+  res.send("hey server is running succesfully");
 })
 //
 app.use("/", TicketBooking);
 app.use("/users", userRouter);
 app.use(express.json());
-app.listen(port, () =>
+app.listen(PORT, () =>
   console.log(
-    `BookMyShow App backend listening on port http://localhost:${port}`
+    `BookMyShow App backend listening on port http://localhost:${PORT}`
   )
 );
 module.exports = app;
